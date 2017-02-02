8c3a3c74fa4c2a06e0060b5eb939b70de955b68f
BluezamX <jasper.wijnhoven@hotmail.com>
2017-02-02 21:31:05 +0100

    Kan je raden welke bestanden door mij zijn geupload?

---
+++ Variables_van_Jasper_de_coolste_persoon_ooit.py
@@ -0,0 +1,57 @@
+visibility_buttonExit = False
+visibility_text = False
+game = None
+init = 0
+function = ""
+Player_Name = ""
+PlayerScore = 0
+Correct = None
+QuestionTimer = 50
+number = 0
+player1 = None
+player1x = 0
+player1y = 0
+win_game = False
+dobbelsteen_draw = False
+startingplace = ''
+antwoord = ""
+color = "Rood"
+
+# Used variables
+# Used in Input.py
+inputloop = True
+numbplayers = 1
+inputpart = 1
+avataroneimage = ""
+avatartwoimage = ""
+avatarthreeimage = ""
+avatarfourimage = ""
+
+# Used in PlayGame.py
+dicenumber = 0
+dicecheck = False
+movement = ""
+answer = False
+questionType = 0
+buttonanswer = ""
+questionint = 0
+buttoncheck = False
+diceroll = True
+dicevisible = False
+questiondone = True
+movecheck = True
+gameloop = True
+exitcheck = False
+gamestart = True
+listcheck = 0
+playerturn = None
+playernumber = 0
+movedone = True
+player1 = None
+player2 = None
+player3 = None
+player4 = None
+Player1Name = ""
+Player2Name = ""
+Player3Name = ""
+Player4Name = ""
